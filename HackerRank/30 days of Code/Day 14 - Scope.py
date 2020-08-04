896baa2d85af8ee2c797c1c07e861c291bd78214
Akshit Gupta <akshitgupta29@gmail.com>
2020-08-05 00:12:33 +0530

    Day 13 completed

--- HackerRank/30 days of Code/Day 14 - Scope.py
+++ HackerRank/30 days of Code/Day 14 - Scope.py
@@ -1,12 +1,21 @@
 class Difference:
+
     def __init__(self, a):
         self.__elements = a
 
+    def computeDifference(self):
+        self.maximumDifference = abs(self.__elements[0] - self.__elements[1])
+
         # Add your code here
+        for i in range(len(self.__elements)):
+            for j in range(i+1, len(self.__elements)):
+                diff = abs(self.__elements[i] - self.__elements[j])
+                if diff > self.maximumDifference:
+                    self.maximumDifference = diff
+        return self.maximumDifference
+
 
 # End of Difference class
-
-
 _ = input()
 a = [int(e) for e in input().split(' ')]
 

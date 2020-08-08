43eb3972905e6b51dbf0f8da4c80b66f27441cfa
Akshit Gupta <akshitgupta29@gmail.com>
2020-08-08 12:47:47 +0530

    day 16 finished

--- HackerRank/30 days of Code/Day 15 - Linked List.py
+++ HackerRank/30 days of Code/Day 15 - Linked List.py
@@ -13,6 +13,8 @@ class Solution:
 
     def insert(self, head, data):
         # Complete this method
+        if head is None:
+            pass
         pass
 
 
---
+++ HackerRank/30 days of Code/Day 16 - Exceptions - String to Integer.py
@@ -0,0 +1,10 @@
+#!/bin/python3
+
+import sys
+
+try:
+    s = input().strip()
+# print (s)
+    print (int(s))
+except:
+    print ("Bad String")
